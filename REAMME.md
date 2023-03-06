dc62e656642e0a10c2eaa3b9fa4d13bd77d4dce3
thaitani <42672817+thaitani@users.noreply.github.com>
2023-03-06 10:38:04 +0900

    feat: 完成

---
+++ REAMME.md
@@ -0,0 +1,12 @@
+# switchbot api token generator
+
+## 事前準備
+- `deno`インストール
+-  `.env`配置
+   - `TOKEN`
+   - `SECRET`
+
+## 実行
+```
+deno run --allow-env --allow-read main.ts
+```
---
+++ main.ts
@@ -0,0 +1,31 @@
+import { encode } from "https://deno.land/std/encoding/base64.ts"
+import "https://deno.land/std@0.178.0/dotenv/load.ts";
+
+const token = Deno.env.get("TOKEN");
+const secret = Deno.env.get("SECRET");
+const t = Date.now();
+
+const nonce = "requestID";
+const data = token + t + nonce;
+
+const encoder = new TextEncoder()
+const keyBuf = encoder.encode(secret);
+
+const key = await crypto.subtle.importKey(
+  "raw",
+  keyBuf,
+  {name: "HMAC", hash: "SHA-256"},
+  true,
+  ["sign", "verify"],
+)
+
+const dataBuf = encoder.encode(data);
+const result = await crypto.subtle.sign("HMAC", key , dataBuf.buffer);
+const sign = encode(new Uint8Array(result))
+
+console.log(`Authorization: ${token}`);
+console.log(`sign: ${sign}`);
+console.log(`nonce: ${nonce}`);
+console.log(`t: ${t}`);
+
+

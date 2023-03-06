e262d62f452ab1dbe6041dc24eaa553f50244735
thaitani <42672817+thaitani@users.noreply.github.com>
2023-03-06 10:45:12 +0900

    style: code format

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # switchbot api token generator
 
 ## 事前準備
+
 - `deno`インストール
--  `.env`配置
-   - `TOKEN`
-   - `SECRET`
+- `.env`配置
+  - `TOKEN`
+  - `SECRET`
 
 ## 実行
+
 ```
 deno run --allow-env --allow-read main.ts
 ```
--- main.ts
+++ main.ts
@@ -1,31 +1,29 @@
-import { encode } from "https://deno.land/std/encoding/base64.ts"
+import { encode } from "https://deno.land/std/encoding/base64.ts";
 import "https://deno.land/std@0.178.0/dotenv/load.ts";
 
-const token = Deno.env.get("TOKEN");
-const secret = Deno.env.get("SECRET");
+const token = Deno.env.get("TOKEN") as string;
+const secret = Deno.env.get("SECRET") as string;
 const t = Date.now();
 
 const nonce = "requestID";
 const data = token + t + nonce;
 
-const encoder = new TextEncoder()
+const encoder = new TextEncoder();
 const keyBuf = encoder.encode(secret);
 
 const key = await crypto.subtle.importKey(
   "raw",
   keyBuf,
-  {name: "HMAC", hash: "SHA-256"},
+  { name: "HMAC", hash: "SHA-256" },
   true,
   ["sign", "verify"],
-)
+);
 
 const dataBuf = encoder.encode(data);
-const result = await crypto.subtle.sign("HMAC", key , dataBuf.buffer);
-const sign = encode(new Uint8Array(result))
+const result = await crypto.subtle.sign("HMAC", key, dataBuf.buffer);
+const sign = encode(new Uint8Array(result));
 
 console.log(`Authorization: ${token}`);
 console.log(`sign: ${sign}`);
 console.log(`nonce: ${nonce}`);
 console.log(`t: ${t}`);
-
-
